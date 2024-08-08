ecab1e169c88c9d03c95c47e125f35fc7ebf7ca9
Gift <gzomandi07@gmail.com>
2024-08-08 14:45:45 +0300

    header complete

--- neighbornest/src/components/Header.jsx
+++ neighbornest/src/components/Header.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
-const Header = () => {
+export default function Header() {
   return (
-    <header className='header'>
+    <header className='header-container'>
       <div className='logo'>
-        <Link to="/">FitFusion</Link>
       </div>
       <nav className='navbar'>
-        <Link to="/">Home</Link>
-        <Link to="/about-us">About Us</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact-us">Contact Us</Link>
-        <button className='login-button'>
-          <Link to="/login">Login</Link>
+        <Link to="/" className='home'>Home</Link>
+        <Link to="/features" className='features'>Features</Link>
+        <Link to="/about-us" className='about-us'>About Us</Link>
+        <Link to="/contact-us" className='contact-us'>Contact Us</Link>
+        <button className='header-button'>
+          <Link to="/login-or-create-account" className='login-create-account'>Login or Create Account</Link>
         </button>
       </nav>
     </header>
   );
-};
-
-export default Header;
+}
