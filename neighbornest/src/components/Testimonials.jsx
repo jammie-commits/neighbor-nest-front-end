35b8ada69c35f8bcce954655e28a13de9c2634a0
Gift <gzomandi07@gmail.com>
2024-08-08 18:06:03 +0300

    add testimonial features

--- neighbornest/src/components/Testimonials.jsx
+++ neighbornest/src/components/Testimonials.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
-function Testimonials() {
+export default function Main() {
   return (
-    <div>Testimonials</div>
-  )
+    <div className='testimonials-container'>
+      <span className='clients-say'>What Our Clients Say</span>
+      <div className='flex-row-b'>
+        <div className='frame'>
+          <div className='frame-1'>
+            <div className='icon-pack'>
+              <div className='vector' />
+              <div className='vector-2' />
+            </div>
+            <div className='frame-3'>
+              <span className='long-established-fact'>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </span>
+            </div>
+          </div>
+          <div className='frame-4'>
+            <div className='avatar'>
+              <div className='ellipse' />
+            </div>
+            <span className='lora-smith'>Lora Smith </span>
+          </div>
+        </div>
+        <div className='frame-5'>
+          <div className='frame-6'>
+            <div className='icon-pack-7'>
+              <div className='vector-8' />
+              <div className='vector-9' />
+            </div>
+            <div className='frame-a'>
+              <span className='reader-layout'>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </span>
+            </div>
+          </div>
+          <div className='frame-b'>
+            <div className='avatar-c'>
+              <div className='ellipse-d' />
+            </div>
+            <span className='lora-smith-e'>Lora Smith </span>
+          </div>
+        </div>
+        <div className='frame-f'>
+          <div className='frame-10'>
+            <div className='icon-pack-11'>
+              <div className='vector-12' />
+              <div className='vector-13' />
+            </div>
+            <div className='frame-14'>
+              <span className='reader-layout-15'>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </span>
+            </div>
+          </div>
+          <div className='frame-16'>
+            <div className='avatar-17'>
+              <div className='ellipse-18' />
+            </div>
+            <span className='lora-smith-19'>Lora Smith </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Testimonials
