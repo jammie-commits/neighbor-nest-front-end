7cb3b391fd43a9cae4686413ff33048e7d6a9e30
Gift <gzomandi07@gmail.com>
2024-08-08 17:22:55 +0300

    finetune services section

--- neighbornest/src/components/Services.jsx
+++ neighbornest/src/components/Services.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
-function Services() {
+export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <div className='services-container'>
+      <span className='services'>Services</span>
+      <div className='services-grid'>
+        <div className='service'>
+          <div className='icon-container'>
+            <div className='bg' />
+            <div className='icon'>
+              <div className='vector' />
+            </div>
+          </div>
+          <div className='text-container'>
+            <span className='service-title'>News Updates</span>
+            <span className='service-description'>
+              Unlock your potential with our expertly designed exercises that
+              cover every aspect of your learning journey.
+            </span>
+          </div>
+        </div>
+        <div className='service-1'>
+          <div className='icon-container-2'>
+            <div className='bg-3' />
+            <div className='icon-4'>
+              <div className='vector-5' />
+            </div>
+          </div>
+          <div className='text-container-6'>
+            <span className='service-title-7'>Organize/Join Events</span>
+            <span className='service-description-8'>
+              Our app tailors workouts to your fitness level and goals, ensuring
+              optimal results and safe progress.
+            </span>
+          </div>
+        </div>
+        <div className='service-9'>
+          <div className='icon-container-a'>
+            <div className='bg-b' />
+            <div className='icon-c'>
+              <div className='vector-d' />
+            </div>
+          </div>
+          <div className='text-container-e'>
+            <span className='service-title-f'>Connect with Neighbors</span>
+            <span className='service-description-10'>
+              Monitor your performance with detailed analytics, helping you stay
+              on track and reach your fitness milestones.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Services
