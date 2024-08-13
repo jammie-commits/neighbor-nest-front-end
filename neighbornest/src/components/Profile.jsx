a2c9b75f7e2f0450b02dea0e2a041f4672578f8d
Gift <gzomandi07@gmail.com>
2024-08-13 13:16:17 +0300

    setup admin profile

--- neighbornest/src/components/Profile.jsx
+++ neighbornest/src/components/Profile.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
 
-function Profile() {
+export default function Main() {
   return (
-    <div></div>
-  )
+    <div className='main-container'>
+      <div className='modern-abstract-logo' />
+      <div className='rectangle'>
+        <div className='rectangle-1'>
+          <div className='left-c'>
+            <div className='group' />
+          </div>
+        </div>
+        <div className='flex-row-c'>
+          <div className='ellipse' />
+          <span className='alexa-rawles'>Alexa Rawles</span>
+          <div className='rectangle-2'>
+            <span className='edit'>Edit</span>
+          </div>
+          <span className='email'>alexarawles@gmail.com</span>
+        </div>
+        <div className='flex-row-db'>
+          <span className='full-name'>Full Name</span>
+          <span className='neighborhood'>Neighborhood</span>
+        </div>
+        <div className='flex-row-acc'>
+          <div className='rectangle-3'>
+            <span className='your-first-name'>Your First Name</span>
+          </div>
+          <div className='rectangle-4'>
+            <span className='your-first-name-5'>Your First Name</span>
+          </div>
+        </div>
+        <div className='flex-row'>
+          <span className='email-6'>Email</span>
+          <span className='block'>Block</span>
+        </div>
+        <div className='flex-row-7'>
+          <div className='rectangle-8'>
+            <span className='your-first-name-9'>Your First Name</span>
+          </div>
+          <div className='rectangle-a'>
+            <span className='your-first-name-b'>Your First Name</span>
+          </div>
+        </div>
+        <div className='flex-row-d'>
+          <span className='gender'>Gender</span>
+          <span className='house-number'>House Numder</span>
+        </div>
+        <div className='flex-row-af'>
+          <div className='rectangle-c'>
+            <span className='your-first-name-d'>Your First Name</span>
+          </div>
+          <div className='rectangle-e'>
+            <span className='your-first-name-f'>Your First Name</span>
+          </div>
+        </div>
+        <span className='my-email-address'>My email Address</span>
+        <div className='flex-row-ea'>
+          <div className='ellipse-10' />
+          <span className='email-11'>alexarawles@gmail.com</span>
+          <div className='sms' />
+        </div>
+        <span className='time-ago'>1 month ago</span>
+      </div>
+      <div className='frame'>
+        <div className='frame-12'>
+          <span className='contacts'>Contacts</span>
+          <div className='frame-13'>
+            <div className='frame-14'>
+              <div className='social-network'>
+                <div className='vector' />
+              </div>
+              <span className='phone-number'>(334) 202-4792</span>
+            </div>
+            <div className='frame-15'>
+              <div className='social-network-16'>
+                <div className='instagram' />
+              </div>
+              <span className='nature-pro'>@nature.pro</span>
+            </div>
+            <div className='frame-17'>
+              <div className='icon-pack'>
+                <div className='vector-18' />
+              </div>
+              <span className='email-19'>haddawy@comcast.net</span>
+            </div>
+          </div>
+        </div>
+        <div className='frame-1a'>
+          <div className='frame-1b'>
+            <span className='address'>
+              998 Devonshire Ave.Camp Hill, PA 17011
+            </span>
+            <span className='location'>Johannesburg, South Africa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Profile
