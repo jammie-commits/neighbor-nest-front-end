0d38829519e329d5697650e30a8f19eeabe3d7a3
princess <princessmumbi001@gmail.com>
2024-08-15 16:10:44 +0300

    updates

--- neighbornest/src/App.jsx
+++ neighbornest/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import DefaultPage from './pages/DefaultPage'; 
 import ContactUs from './components/ContactUs/ContactUs'; 
 import AboutUs from './components/AboutUs/AboutUs'; 
 import Dashboard from './components/Superadmin/Dashboard';
@@ -13,12 +12,10 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<DefaultPage />} /> 
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/dashboard/*" element={<Dashboard />} /> 
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<DefaultPage />} /> 
         </Routes>
       </div>
     </Router>
