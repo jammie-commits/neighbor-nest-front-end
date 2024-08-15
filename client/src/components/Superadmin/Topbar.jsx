edf2b3abf858194baec8efb34e4b04fa625214f8
Gift <gzomandi07@gmail.com>
2024-08-15 21:33:57 +0300

    resolve conflicts in staging

--- client/src/components/Superadmin/Topbar.jsx
+++ client/src/components/Superadmin/Topbar.jsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
-import './Topbar.css';
-
-const Topbar = () => {
-    return (
-        <div className="topbar">
-            <input type="text" placeholder="Search neighborhood, admins..." />
-            <div className="topbar-right">
-                <div className="notifications">
-                    <FontAwesomeIcon icon={faBell} />
-                </div>
-                <Link to="/profile" className="user-info">
-                    <span>Katie</span>
-                    <img src="user_profile_image_url" alt="User" />
-=======
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -41,7 +22,6 @@ const Topbar = () => {
                 </Link>
                 <Link to="/profile" className={styles['user-info']}>
                     <img src={profileImage} alt="User" />
->>>>>>> feature/mumbi
                 </Link>
             </div>
         </div>
